1e24a7febcc44a486c6e6da628a97acf1bb34577
John Behm <jxsl13@googlemail.com>
2023-12-19 14:33:35 +0100

    pass db handler to bot

--- bot/bot.go
+++ bot/bot.go
@@ -2,36 +2,66 @@ package bot
 
 import (
 	"context"
+	"database/sql"
+	"log"
 
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
+	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/state"
+	"github.com/diamondburned/arikawa/v3/utils/json/option"
 )
 
 type Bot struct {
 	state *state.State
+	db    *sql.DB
 }
 
-func New(token string) (*Bot, error) {
+// New requires a discord bot token and returns a Bot instance.
+// A bot token starts with Nj... and can be obtained from the discord developer portal.
+func New(token string, db *sql.DB) (*Bot, error) {
 	s := state.New("Bot " + token)
 	bot := &Bot{
 		state: s,
+		db:    db,
 	}
+
+	s.AddHandler(func(*gateway.ReadyEvent) {
+		me, _ := s.Me()
+		log.Println("connected to the gateway as", me.Tag())
+	})
 
 	r := cmdroute.NewRouter()
 
 	r.AddFunc("ping", bot.Ping)
+	r.AddFunc("register", bot.registerServer)
 
 	s.AddInteractionHandler(r)
 	s.AddIntents(
-		gateway.IntentGuilds | gateway.IntentGuildMessages | gateway.IntentGuildMessageReactions | gateway.IntentGuildMessageTyping | gateway.IntentDirectMessages | gateway.IntentDirectMessageReactions | gateway.IntentDirectMessageTyping | gateway.IntentMessageContent,
+		gateway.IntentGuilds,
 	)
 
 	err := cmdroute.OverwriteCommands(s, []api.CreateCommandData{
 		{
 			Name:        "ping",
 			Description: "Ping!",
+		},
+		{
+			Name:        "register",
+			Description: "Register a server",
+			Options: []discord.CommandOption{
+				&discord.StringOption{
+					OptionName:  "address",
+					Description: "ipv4:port or [ipv6]:port",
+					MinLength:   option.NewInt(16),
+					MaxLength:   option.NewInt(64),
+					Required:    true,
+				},
+			},
+			/*DefaultMemberPermissions: discord.NewPermissions(
+				discord.PermissionAdministrator,
+			),*/
 		},
 	})
 	if err != nil {
@@ -47,4 +77,12 @@ func (b *Bot) Connect(ctx context.Context) error {
 
 func (b *Bot) Close() error {
 	return b.state.Close()
+}
+
+func errorResponse(err error) *api.InteractionResponseData {
+	return &api.InteractionResponseData{
+		Content:         option.NewNullableString("**Error:** " + err.Error()),
+		Flags:           discord.EphemeralMessage,
+		AllowedMentions: &api.AllowedMentions{ /* none */ },
+	}
 }
